dd42993c1c3de0ba2c4b55f0f765b25ad5b9115b
Pascal Guenther <pascal.develop@pguenther.net>
2020-05-21 00:21:03 +0200

    added WiFiManager and DoubleResetDetector

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,53 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h> 
+#include <DoubleResetDetector.h>
+#include "project-config.h"
+#include "bsp-config.h"
+#include <WiFiManager.h>
+
+// ------- Static Variables -------
+DoubleResetDetector oDrd(DRD_CFG_TIMEOUT_SECONDS, DRD_CFG_TIMEOUT_ADDRESS);
+
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(SERIAL_BAUDRATE);
+  Serial.println("\n\n\n");
+  Serial.println("============================");
+  Serial.println("       System start         ");
+  Serial.println("============================");
+
+
+  // WiFi Manager will be freed after configuration has finished
+  WiFiManager wifiManager;
+
+  // Start a configuration portal, if not network config is present.
+  // Otherwise, try to connect to the WiFi network
+  bool bWiFiConnected;
+  if(oDrd.detectDoubleReset()) {
+    // device has been double-reset in a short amount of time
+    Serial.println("Config mode after reset");
+    bWiFiConnected = wifiManager.startConfigPortal(WIFI_CFG_ACCESS_POINT_SSID, WIFI_CFG_ACCESS_POINT_PASSWORD);
+  } else {
+    bWiFiConnected = wifiManager.autoConnect(WIFI_CFG_ACCESS_POINT_SSID, WIFI_CFG_ACCESS_POINT_PASSWORD);
+  }
+  oDrd.stop();
+
+  if (!bWiFiConnected) {
+    // failed to connect to an AP
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    ESP.reset();
+    delay(5000);
+  }
+
+  Serial.println("Connected to WiFi \"" + WiFi.SSID() + "\", IP " + WiFi.localIP().toString());
+
+  Serial.printf("Leaving setup...");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
---
+++ src/project-config.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Arduino.h"
+
+// #define NDEBUG
+
+
+#ifdef LED_BUILTIN
+    #undef LED_BUILTIN
+#endif // LED_BUILTIN
+
+#define NODEMCU_V1 (1u)
+#define NODEMCU_V2 (2u)
+#define NODEMCU_V3 (3u)
+
+#define BOARD_TYPE NODEMCU_V3
+
+#define SERIAL_BAUDRATE (115200u)
+
+// At first start, the device will create an Access Point WiFi network (Captive portal).
+// Connect to this network to configure the credentials of your WiFi network.
+// Below, please configure the credentials of the Access Point that the device will create.
+#define WIFI_CFG_ACCESS_POINT_SSID        ("Garage")
+#define WIFI_CFG_ACCESS_POINT_PASSWORD    ("456config789")
+
+// uncomment to set a minimum threadhold for signal quality of the AP to connect to
+//#define WIFI_CFG_MINIMUM_SIGNAL_QUALITY_PERCENT (8)
+
+// uncomment to set a timeout after which the configuration portal will close
+// #define WIFI_CFG_PORTAL_TIMEOUT_SECONDS (4*60)
+
+// Double reset detector: Timeout
+#define DRD_CFG_TIMEOUT_SECONDS       (10u)
+
+// Double reset detector: Address
+#define DRD_CFG_TIMEOUT_ADDRESS       (0u)
